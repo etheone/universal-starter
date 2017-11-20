48448f5dbb67ff9cd177563974353a95498e03f1
Emil Nilsson <emilnilsson@Emils-MacBook-Pro.local>
2017-11-20 13:47:39 +0100

    Updated<2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF, Location } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { ExtraComponent } from './extra.component';
 import { HomeComponent } from './home/home.component';
+import { getBaseLocation } from './shared/common-function.util';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,11 @@ import { HomeComponent } from './home/home.component';
       { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
     ])
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF,
+    useFactory: getBaseLocation 
+  }
+  ],
   bootstrap: [AppComponent, ExtraComponent]
 })
 export class AppModule { }
